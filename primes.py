684f5a8d885a782004b762ea85c49b69a8fcb0b8
jummanabedin <91836600+jummanabedin@users.noreply.github.com>
2022-10-22 14:35:58 +0100

    primes

--- primes.py
+++ primes.py
@@ -1,6 +1,16 @@
 """List of prime numbers generator."""
-"""ENTER YOUR SOLUTION HERE! test push"""
+"""ENTER YOUR SOLUTION HERE!"""
+
 
 def primes(number_of_primes):
     list = []
+    n = 2
+    while len(list) != number_of_primes:
+        for i in range(2, n // 2 + 1):
+            if n % i == 0:
+                break
+        else:
+            list.append(n)
+        n += 1
+
     return list
